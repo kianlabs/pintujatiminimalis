e769d49c7758d33c63ac36c1e1a6970ef37352d1
ridzkyan0504-arch <ridzkyan0504@gmail.com>
2026-09-02 15:12:42 +0700

    feat: sitemap, favicon, OG image, fix JSON-LD telephone

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@tailwindcss/vite'
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://pintujatiminimalis.my.id',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwind()],
   },
